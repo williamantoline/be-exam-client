c8e89254cd36c7c2dd0e84fc9658cd351a9595b6
William Antoline <65105895+williamantoline@users.noreply.github.com>
2023-02-01 21:24:12 +0700

    fix home

--- src/components/pages/user/home/home.tsx
+++ src/components/pages/user/home/home.tsx
@@ -6,7 +6,6 @@ export default function UserHome(props:Props){
     const isBackgroundRed = true;
     return(
         <>
-        
             <header>
                 <div className="bg-dark collapse" id="navbarHeader">
                     <div className="container">
@@ -18,9 +17,9 @@ export default function UserHome(props:Props){
                             <div className="col-sm-4 offset-md-1 py-4">
                                 <h4 className="text-white">Contact</h4>
                                 <ul className="list-unstyled">
-                                    <li><a href="#" className="text-white">Follow on Twitter</a></li>
-                                    <li><a href="#" className="text-white">Like on Facebook</a></li>
-                                    <li><a href="#" className="text-white">Email me</a></li>
+                                    <li><a href="/" className="text-white">Follow on Twitter</a></li>
+                                    <li><a href="/" className="text-white">Like on Facebook</a></li>
+                                    <li><a href="/" className="text-white">Email me</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -28,7 +27,7 @@ export default function UserHome(props:Props){
                 </div>
                 <div className="navbar navbar-dark bg-dark shadow-sm">
                     <div className="container">
-                        <a href="#" className="navbar-brand d-flex align-items-center">
+                        <a href="/" className="navbar-brand d-flex align-items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" className="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
                             <strong>Album</strong>
                         </a>
@@ -45,8 +44,8 @@ export default function UserHome(props:Props){
                             <h1 className="fw-light">Album example</h1>
                             <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
                             <p>
-                                <a href="#" className="btn btn-primary my-2">Main call to action</a>
-                                <a href="#" className="btn btn-secondary my-2">Secondary action</a>
+                                <a href="/" className="btn btn-primary my-2">Main call to action</a>
+                                <a href="/" className="btn btn-secondary my-2">Secondary action</a>
                             </p>
                         </div>
                     </div>
